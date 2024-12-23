9c09a3a39a075aff3210a470f0053025a2338d1c
Antonio <acgomesn@gmail.com>
2024-12-22 22:41:13 -0300

    creating bodyParser helper

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -26,25 +26,18 @@ module.exports = {
   },
 
   createUser(request, response) {
-    let body = '';
+    const { body } = request;
 
-    request.on('data', (chunk) => {
-      body += chunk
-    });
+    const lastUserId = users[users.length - 1].id;
 
-    request.on('end', () => {
-      body = JSON.parse(body)
+    const newUser =
+    {
+      id: lastUserId + 1,
+      name: body.name,
+    }
 
-      const lastUserId = users[users.length - 1].id;
-      const newUser =
-      {
-        id: lastUserId + 1,
-        name: body.name,
-      }
+    users.push(newUser);
 
-      users.push(newUser);
-
-      response.send(200, newUser);
-    });
+    response.send(200, newUser);
   },
 }
